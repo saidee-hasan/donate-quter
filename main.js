cd229bb88f7a58cfaa1c17fc8eebc5234d73e48b
saideeWD <mdsaideehasan@gmail.com>
2024-09-25 07:40:33 +0600

    my modal done

--- main.js
+++ main.js
@@ -1,60 +1,69 @@
-document.getElementById("donate").addEventListener('click',function(e){
-    e.preventDefault;
-    
-    const addAmount = document.getElementById('donate-amount').value;
-    const totalAmount = document.getElementById("total-amount").innerText;
-    const myBalance = document.getElementById("myBalance").innerText;
+document.getElementById("donate").addEventListener("click", function (e) {
+  e.preventDefault;
 
+  const donateAmount = document.getElementById("donate-amount").value;
+  const totalAmount = document.getElementById("total-amount").innerText;
+  const myBalance = document.getElementById("myBalance").innerText;
 
+  if (donateAmount > 0 && parseFloat(myBalance) >= donateAmount) {
+    const amountAdd = parseFloat(donateAmount) + parseFloat(totalAmount);
+    document.getElementById("total-amount").innerText = amountAdd;
 
-    if(addAmount > 0 && parseFloat(myBalance )>=  addAmount ){
-        const amountAdd = parseFloat(addAmount) + parseFloat(totalAmount)
-        document.getElementById('total-amount').innerText = amountAdd;
+    const balance = parseFloat(myBalance) - donateAmount;
+    document.getElementById("myBalance").innerText = balance;
 
-        const balance = parseFloat(myBalance) - addAmount;
-        document.getElementById('myBalance').innerText = balance;
-        
-         // Modal Interface
-        document.getElementById("modal-text").innerText ="Congrats !";
-        const donate_title = document.getElementById('donate-title').innerText;
-        console.log(donate_title)
-         const time = new Date();
-         const dateTime =   document.innerText = time;
-         const div = document.createElement('div');
-         div.innerHTML =`
+    // Modal Interface
+    document.getElementById("modal-text").innerText = "Congrates !";
+    document.getElementById("modal-p-text").innerText = "you have donate ";
+
+    const donate_title = document.getElementById("donate-title").innerText;
+    console.log(donate_title);
+    const time = new Date();
+    const dateTime = (document.innerText = time);
+    const div = document.createElement("div");
+    div.innerHTML = `
          <div class="border border-black p-3 rounded-md mt-5">
-          <h3> ${parseFloat(addAmount)} Taka is   ${donate_title}</h3>
+          <h3 class='font-bold'> ${parseFloat(
+            donateAmount
+          )} Taka is   ${donate_title}</h3>
       
-         <p>${dateTime}</p>
+         <p class="text-gray-400">${dateTime}</p>
          </div>
-        
-         
-         `
+         `;
 
-         document.getElementById('ty').appendChild(div);
+    document.getElementById("ty").appendChild(div);
 
-        
-       
-      console.log(lll)
+    console.log(lll);
+  } else {
+    document.getElementById("modal-text").innerText = "Inalid!";
+  }
+});
 
-    }else{
-       document.getElementById("modal-text").innerText ="Inalid!"
-    }
+// Active Color
 
-})
+function showSectionBtn(id) {
+  document.getElementById("card-section").classList.add("hidden");
+  document.getElementById("history").classList.add("hidden");
+  document.getElementById(id).classList.remove("hidden");
+}
+document.getElementById("btn").addEventListener("click", function () {
+  showSectionBtn("card-section");
+});
+document.getElementById("btn-2").addEventListener("click", function () {
+  showSectionBtn("history");
+});
 
+// Btn Active Color
+function showBtnColor(id) {
+  document.getElementById(id).classList.add("bg-green-400");
+  document.getElementById(id).classList.add("bg-green-400");
+}
 
-document.getElementById("btn").addEventListener("click",function(){
-    document.getElementById("history").classList.add("hidden")
-    document.getElementById("card-section").classList.remove("hidden")
-    
-
-    
-    })
-document.getElementById("btn-2").addEventListener("click",function(){
-   
-    document.getElementById("history").classList.remove("hidden")
-    document.getElementById("card-section").classList.add("hidden")
-    })
-
-
+document.getElementById("btn").addEventListener("click", function () {
+  document.getElementById("btn-2").classList.remove("bg-green-400");
+  showBtnColor("btn")
+});
+document.getElementById("btn-2").addEventListener("click", function () {
+  document.getElementById("btn").classList.remove("bg-green-400");
+  showBtnColor("btn-2")
+});
